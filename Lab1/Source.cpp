2ad7a416080c84cf66996816083f8f0cce0c13a7
Olesia <5otaisora@gmail.com>
2017-10-06 22:40:46 +0300

    Update and rename  Source.cpp to Source.cpp

--- Lab1/Source.cpp
+++ Lab1/Source.cpp
@@ -23,7 +23,7 @@ int main()
 	doubleResult.mantissa = binaryNotation(&a, 52);
 	std::cout << doubleResult.sign << " " << doubleResult.precision << " " << doubleResult.mantissa << std::endl;
 
-	unsigned int b = 36; //Ðàçìåð â áàéòàõ çàâèñèò îò ñèñòåìû
+	unsigned int b = 36; //Размер в байтах зависит от системы
 	std::cout << binaryNotation(&b, sizeof(typeid(b).name())*8) << std::endl;
 
 	system("pause");
@@ -32,21 +32,21 @@ int main()
 
 
 template<class T> std::string binaryNotation(T* object, int countOfBytes) {
-	int intPath = abs((int)*object); //Îòñå÷åíèå äðîáíîé ÷àñòè (åñëè èìååòñÿ)
+	int intPath = abs((int)*object); //Отсечение дробной части (если имеется)
 	std::string result;
 	char tmpChar[2];
-	while (intPath) { //Ïåðåâîä öåëîé ÷àñòè â äâîè÷íóþ ñ.ñ.
-		sprintf(tmpChar, "%d", intPath % 2); //Ìîæíî îáîáùèòü íà ëþáóþ ñ.ñ., åñëè îñòàòîê îò äåëåíèÿ îäíîçíà÷íîå ÷èñëî
+	while (intPath) { //Перевод целой части в двоичную с.с.
+		sprintf(tmpChar, "%d", intPath % 2); //Можно обобщить на любую с.с., если остаток от деления однозначное число
 		result += tmpChar[0]; 
 		intPath /= 2;
 	}
-	std::reverse(result.begin(), result.end()); //Îáðàòíàÿ çàïèñü
+	std::reverse(result.begin(), result.end()); //Обратная запись
 
-	unsigned short expMantissa = (unsigned short) result.size() - 1; //Îïðåäåëåíèå èçìåíåíèÿ ïîðÿäêà ïðè íîðìàëèçàöèè ìàíòèññû
+	unsigned short expMantissa = (unsigned short) result.size() - 1; //Определение изменения порядка при нормализации мантиссы
 	std::string fracResult;
-	if (typeid(T) == typeid(double) || typeid(T) == typeid(long double) || typeid(T) == typeid(float)) {  // Ïðîâåðêà óñëîâèÿ íà âîçìîæíîå íàëè÷èå äðîáíîé ÷àñòè ... Èëè ëó÷øå èñïîëüçîâàòü typeid(T).name()?
+	if (typeid(T) == typeid(double) || typeid(T) == typeid(long double) || typeid(T) == typeid(float)) {  // Проверка условия на возможное наличие дробной части ... Или лучше использовать typeid(T).name()?
 		T fracPath = fabs(*object - (int) *object);
-		unsigned short countOfNumbers = 10; //Íà ñëó÷àé, åñëè áóäåò áåñêîíå÷íàÿ äðîáü
+		unsigned short countOfNumbers = 10; //На случай, если будет бесконечная дробь
 		while (fracPath && countOfNumbers) {
 			fracPath *= 2;
 			sprintf(tmpChar, "%d", (int)fracPath);
@@ -56,7 +56,7 @@ template<class T> std::string binaryNotation(T* object, int countOfBytes) {
 		}
 		result += fracResult;
 		expMantissa += 1023;
-		doubleResult.precision = binaryNotation(&expMantissa, 11); //Îïðåäåëåííî ïîêà òîëüêî äëÿ double. Ïî èäåå ìîæíî îáîáùèòü
+		doubleResult.precision = binaryNotation(&expMantissa, 11); //Определенно пока только для double. По идее можно обобщить
 	}	
 	result += std::string(countOfBytes - result.size(), '0');
 	return result;
